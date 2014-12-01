cf0adfdf6c854c335e2090782aa409019acb4034
Matthew Toups <mtoups@mit.edu>
2014-12-01 15:36:56 -0600

    Add comments to SubmitCommand.sh in library build tools.

--- PhotonPropagation/LibraryBuildTools/SubmitCommand.sh
+++ PhotonPropagation/LibraryBuildTools/SubmitCommand.sh
@@ -2,6 +2,16 @@
 
 #Real job
 jobsub --opportunistic --X509_USER_PROXY /scratch/bjpjones/grid/bjpjones.uboone.proxy -g -N 9375 -dOUT /uboone/data/users/bjpjones/OpticalProduction  -q OpticalLibraryBuild_Grid.sh `whoami` `pwd`
+#jobsub [Client Options] user_script [user_script_args]
+#--opportunistic
+#--X509_USER_PROXY=/scratch/bjpjones/grid/bjpjones.uboone.proxy
+#-g = grid
+#-N = number of jobs
+#-dOUT dir = output directory
+#-q = mail_on_error
+#user_script = OpticalLibraryBuild_Grid.sh
+#`whoami`=user_name
+#`pwd`=working directory
 
 #Partial job
 #jobsub --opportunistic --X509_USER_PROXY /scratch/bjpjones/grid/bjpjones.uboone.proxy -g -N 1500 -dOUT /uboone/data/users/bjpjones/OpticalProduction  -q OpticalLibraryBuild_Grid.sh `whoami` `pwd`
