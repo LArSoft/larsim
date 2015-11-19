de6e0c243f33e17c8c84b7985100653e7a63641f
Katherine Woodruff <kwoodruf@uboonegpvm06.fnal.gov>
2015-11-19 11:37:55 -0600

    Change dirt filter default to keep cryostat events

--- SimFilters/FilterNoDirtNeutrinos_module.cc
+++ SimFilters/FilterNoDirtNeutrinos_module.cc
@@ -84,7 +84,7 @@ namespace simfilter {
   FilterNoDirtNeutrinos::FilterNoDirtNeutrinos(fhicl::ParameterSet const& pset) :
     fLArG4ModuleLabel    (pset.get< std::string > ("LArG4ModuleLabel"   , "NoLabel")       )
     , fGenModuleLabel    (pset.get< std::string > ("GenModuleLabel"  , "NoLabel")       )
-    , fKeepCryostatNeutrinos    (pset.get< bool > ("KeepCryostatNeutrinos", false)      )
+    , fKeepCryostatNeutrinos    (pset.get< bool > ("KeepCryostatNeutrinos", true)      )
   {
     this->reconfigure(pset);
   }
