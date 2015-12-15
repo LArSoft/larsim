7284c888331d06ea12290ca64a0574a31127810e
Joseph Zennamo <jzennamo@uchicago.edu>
2015-12-15 17:37:43 -0600

    cleaning up, adding useful comments

--- MCSTReco/MCTrackRecoAlg.cxx
+++ MCSTReco/MCTrackRecoAlg.cxx
@@ -92,15 +92,14 @@ namespace sim {
       for(auto const& step_trk : mini_track){
 
         if( int(&step_trk - &mini_track[0])+1 == int(mini_track.size()) ){  //annoying way to check if this is last step
-	  std::cout << "\t\t\t::: TRACK END!" << std::endl;
 	  break;}
 	
 	
 	auto const& nxt_step_trk = mini_track.at(int(&step_trk - &mini_track[0])+1);      	
 
-	//Defining dEdx	
+	//Defining the track step-by-step dEdx and dQdx
 	
-	//Find the distance step-to-step
+	//Find the distance between two adjacent MCSteps
 	double dist = sqrt(pow(step_trk.X() - nxt_step_trk.X(),2) + 
 			   pow(step_trk.Y() - nxt_step_trk.Y(),2) +
 			   pow(step_trk.Z() - nxt_step_trk.Z(),2));
@@ -121,38 +120,38 @@ namespace sim {
 	c = nxt_step_trk.Z() - step_trk.Z();
 	d = -1*(a*step_trk.X() + b*step_trk.Y() + c*step_trk.Z());	
 	
-	//Make a line connecting the two points 
+	//Make a line connecting the two points and find the distance from that line 
+	// 
+	//                        [A dot B]^2     [A dot B]^2
+	// distance^2 = A^2 - 2* ____________ + ______________
+	//                            B^2             B^2	
 	// Test point == x_0
 	// First Step == x_1
 	// Next step == x_2
 	// A = x_1 - x_0
 	// B = x_2 - x_1
 
-	// B definition 
+	// 'B' definition 
 	TVector3 B(nxt_step_trk.Position().X() - step_trk.Position().X(), 
 		   nxt_step_trk.Position().Y() - step_trk.Position().Y(),
 		   nxt_step_trk.Position().Z() - step_trk.Position().Z());
 	
 
-
+	//Initialize the step-by-step dEdx and dQdx containers
 	double step_dedx = 0; 
 	std::vector<double> step_dqdx(3);
 	step_dqdx.clear();
 
-      
+	//Iterate through all the energy deposition points
 	for(auto const& edep : edeps){
-	  // x_0 definition
+	  // 'x_0' definition
 	  TVector3 x_0(edep.pos._x, edep.pos._y, edep.pos._z);
-	  // A definition 
+	  // 'A' definition 
 	  TVector3 A(step_trk.Position().X() - x_0.X(), 
 		     step_trk.Position().Y() - x_0.Y(), 
 		     step_trk.Position().Z() - x_0.Z());
 	  
 	  // Distance from the line connecting x_1 and x_2
-	  // 
-	  //                 [A dot B]^2     [A dot B]^2
-	  // d^2 = A^2 - 2* ____________ + ______________
-	  //                    B^2             B^2
 	  double LineDist = 0;
 	 
 	  if(B.Mag2() != 0){
@@ -160,11 +159,12 @@ namespace sim {
 	  }
 	  else{LineDist = 0;}
 	  
-	  //Planar Distance
+	  //Planar Distance and Radial Line Distance Cuts 
 	  if( (a*edep.pos._x + b*edep.pos._y + c*edep.pos._z + d)/sqrt( pow(a,2) + pow(b,2) + pow(c,2)) <= dist &&
 	      (a*edep.pos._x + b*edep.pos._y + c*edep.pos._z + d)/sqrt( pow(a,2) + pow(b,2) + pow(c,2)) >= 0 && 
 	      LineDist < 0.1){
-	    
+
+	    //dEdx Calculation 
 	    int npid = 0;
 	    double engy = 0;
 	    
@@ -176,115 +176,28 @@ namespace sim {
 	    step_dedx += engy;
 	  }
 	  
-	  // Charge
+	  // dQdx Calculation
 	  auto q_iter = edep.charge.find(edep.pid);
 	  if(q_iter != edep.charge.end())
 	    step_dqdx[edep.pid.Plane] += (double)((*q_iter).second);	  
 	  
 	}
 
+	// Normalize to the distance between the MCSteps 
 	step_dedx /= dist;
 	step_dqdx[0] /= dist;
 	step_dqdx[1] /= dist;
 	step_dqdx[2] /= dist;	
 
+	// Build the vector(s) to add to data product
 	dEdx.push_back(step_dedx);
 	dQdx.push_back(step_dqdx);
 
       }
+      //Add calorimetry to the data product
       mini_track.dEdx(dEdx);
       mini_track.dQdx(dQdx);
-      /*
 
-      auto const& edep_index = edep_v.TrackToEdepIndex(mini_part._track_id);
-      auto const& g4_step_v  = mini_part._det_path;
-      if(edep_index>=0 && g4_step_v.size()) {
-
-	// Get energy deposition vector
-	auto const& edeps = edep_v.GetEdepArrayAt(edep_index);
-
-	auto const& init_vtx = g4_step_v[0].first;
-	auto const& init_mom = g4_step_v[0].second;
-
-	// Initialize "previous step" holder 
-	std::vector<double> previous_vtx(4,0);
-	std::vector<double> previous_mom(4,0);
-	for(size_t i=0; i<4; ++i){
-	  previous_vtx[i] = init_vtx[i];
-	  previous_mom[i] = init_mom[i];
-	}
-	double mass = sqrt(pow(init_mom[3],2) -
-			   pow(init_mom[2],2) -
-			   pow(init_mom[1],2) -
-			   pow(init_mom[0],2));
-
-	std::deque<size_t> edep_list;
-	for(size_t i=0; i<edeps.size(); ++i) {
-	  auto const& edep = edeps[i];
-	  if(edep.energy>0 && part_v.InDetector(edep.x,edep.y,edep.z))
-	    edep_list.push_back(i);
-	}
-	std::deque<size_t>::iterator iter, max_iter;
-
-	double min_dist2 = ::sim::kINVALID_DOUBLE;
-
-	TLorentzVector vtx,mom;
-	mini_track.reserve(edep_list.size());
-	while(edep_list.size()) {
-
-	  min_dist2 = ::sim::kINVALID_DOUBLE;
-	  for(iter = edep_list.begin(); iter != edep_list.end(); ++iter) {
-	    
-	    auto const& edep = edeps[(*iter)];
-	    double dist2 = ( pow(edep.x - previous_vtx[0],2) +
-			     pow(edep.y - previous_vtx[1],2) +
-			     pow(edep.z - previous_vtx[2],2) );
-	    if(dist2 < min_dist2) {
-	      min_dist2 = dist2;
-	      max_iter = iter;
-	    }
-	  }
-	  
-	  if(min_dist2 != ::sim::kINVALID_DOUBLE) {
-
-	    min_dist2 = sqrt(min_dist2);
-	    auto const& edep = edeps[(*max_iter)];
-	    
-	    vtx[0] = edep.x;
-	    vtx[1] = edep.y;
-	    vtx[2] = edep.z;
-	    vtx[3] = (min_dist2/100. / 2.998e8)*1.e9 + previous_vtx[3];
-	    
-	    mom[3] = previous_mom[3] - edep.energy;
-	    if(mom[3]<mass) throw cet::exception(__FUNCTION__) << "Negative energy!"<<previous_vtx[3]<<" - "<<edep.energy<< " < "<<mass;
-	    double momentum_magnitude = sqrt(pow(mom[3],2)-pow(mass,2));
-	    
-	    mom[0] = vtx[0] - previous_vtx[0];
-	    mom[1] = vtx[1] - previous_vtx[1];
-	    mom[2] = vtx[2] - previous_vtx[2];
-	    
-	    double dir_magnitude = sqrt(pow(mom[0],2)+pow(mom[1],2)+pow(mom[2],2));
-	    if(dir_magnitude==0.) throw cet::exception(__FUNCTION__) << "Zero magnitude!" << edep.energy << " : " <<dir_magnitude;
-	    mom[0] *= momentum_magnitude/dir_magnitude;
-	    mom[1] *= momentum_magnitude/dir_magnitude;
-	    mom[2] *= momentum_magnitude/dir_magnitude;
-	    
-	    mini_track.push_back(MCStep(vtx,mom));
-	    
-	    for(size_t i=0; i<4; ++i) {
-	      previous_vtx[i] = vtx[i];
-	      previous_mom[i] = mom[i];
-	    }
-
-	    edep_list.erase(max_iter);
-	    
-	  }else 
-	    
-	    break;
-	  
-	}
-      }
-      */
       fMCTrack.push_back(mini_track);
     }
     
