9aff00814b2b7351641a48c96dfcc4f5df6263a5
Jason Stock <jstock@dunebuild01.fnal.gov>
2016-12-01 23:19:37 -0600

    Merging PhotonBackTracker in

---
+++ larsim/MCCheater/PhotonBackTrackerLoader_module.cc
@@ -0,0 +1,64 @@
+#ifndef PhotonBackTrackerLoader_h
+#define PhotonBackTrackerLoader_h
+////////////////////////////////////////////////////////////////////////
+// Class:       PhotonBackTrackerLoader
+// Module Type: producer
+// File:        PhotonBackTrackerLoader.h
+//
+// Generated at Thu Jun 14 06:49:31 2012 by Brian Rebel using artmod
+// from art v1_00_11.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+
+#include "larsim/MCCheater/PhotonBackTracker.h"
+
+namespace cheat {
+  class PhotonBackTrackerLoader;
+}
+
+class cheat::PhotonBackTrackerLoader : public art::EDProducer {
+public:
+  explicit PhotonBackTrackerLoader(fhicl::ParameterSet const & p);
+  virtual ~PhotonBackTrackerLoader();
+
+  virtual void produce(art::Event & e);
+
+
+private:
+
+  // Declare member data here.
+
+};
+
+//------------------------------------------------------------------------------
+cheat::PhotonBackTrackerLoader::PhotonBackTrackerLoader(fhicl::ParameterSet const & /*p*/)
+{
+  // Call appropriate Produces<>() functions here.
+}
+
+//------------------------------------------------------------------------------
+cheat::PhotonBackTrackerLoader::~PhotonBackTrackerLoader()
+{
+  // Clean up dynamic memory and other resources here.
+}
+
+//------------------------------------------------------------------------------
+// the sole purpose of this module is to issue the Rebuild command to the
+// PhotonBackTracker service.  It should be put after all simulation data producing
+// modules have run in the job, and only in jobs that create the simulation and
+// then make use of the PhotonBackTracker in either cheating reconstruction modules
+// or analyzers
+void cheat::PhotonBackTrackerLoader::produce(art::Event & e)
+{
+  art::ServiceHandle<cheat::PhotonBackTracker> pbt;
+  pbt->Rebuild(e);
+
+  return;
+}
+
+
+DEFINE_ART_MODULE(cheat::PhotonBackTrackerLoader)
+
+#endif /* PhotonBackTrackerLoader_h */
