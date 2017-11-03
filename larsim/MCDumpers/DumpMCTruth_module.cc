3b6695f17a2c4eb1faab3b55faf4081f5804ce60
Gianluca Petrillo <petrillo@fnal.gov>
2017-11-03 18:33:53 -0500

    Utility functions to dump simb objects on screen

--- larsim/MCDumpers/DumpMCTruth_module.cc
+++ larsim/MCDumpers/DumpMCTruth_module.cc
@@ -10,6 +10,8 @@
  *
  */
 
+// LArSoft libraries
+#include "MCDumperUtils.h"
 
 // nutools libraries
 #include "nusimdata/SimulationBase/MCTruth.h"
@@ -27,17 +29,9 @@
 #include "fhiclcpp/types/OptionalSequence.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
-// ROOT
-#include "TDatabasePDG.h"
-#include "TParticlePDG.h"
 
-
-namespace sim {
-  class DumpMCTruth;
-} // namespace sim
-
-namespace {
-  using namespace fhicl;
+class sim::DumpMCTruth: public art::EDAnalyzer {
+    public:
   
   /// Collection of configuration parameters for the module
   struct Config {
@@ -63,12 +57,6 @@ namespace {
     
   }; // struct Config
   
-  
-} // local namespace
-
-
-class sim::DumpMCTruth: public art::EDAnalyzer {
-    public:
   /// Type to enable module parameters description by _art_.
   using Parameters = art::EDAnalyzer::Table<Config>;
   
@@ -108,18 +96,6 @@ class sim::DumpMCTruth: public art::EDAnalyzer {
   /// Returns the name of the product in the form `"module_instance_process"`.
   template <typename Handle>
   static std::string productName(Handle const& handle);
-  
-  /// Returns a string representing the specified origin.
-  static std::string TruthOriginName(simb::Origin_t origin);
-  
-  /// Returns a string representing the specified process.
-  static std::string TruthCCNCname(int ccnc);
-  
-  /// Returns a string representing the specified interaction type.
-  static std::string TruthInteractionTypeName(int type);
-  
-  /// Returns a string with the name of particle the specified with PDG ID.
-  static std::string ParticleName(int pigid);
   
   
     private:
@@ -454,106 +430,6 @@ std::string sim::DumpMCTruth::productName(Handle const& handle) {
     + '_' + prov->processName()
     ;
 } // sim::DumpMCTruth::productName()
-
-
-//------------------------------------------------------------------------------
-std::string sim::DumpMCTruth::TruthOriginName(simb::Origin_t origin) {
-  switch (origin) {
-    case simb::kUnknown          : return "unknown origin";
-    case simb::kBeamNeutrino     : return "neutrinos from beam";
-    case simb::kCosmicRay        : return "cosmic rays";
-    case simb::kSuperNovaNeutrino: return "supernova neutrinos";
-    case simb::kSingleParticle   : return "single particle";
-    default:        return "unsupported (" + std::to_string((int)origin) + ")";
-  } // switch
-} // sim::DumpMCTruth::TruthOriginName()
-
-//------------------------------------------------------------------------------
-std::string sim::DumpMCTruth::TruthCCNCname(int ccnc) {
-  switch (ccnc) {
-    case simb::kCC: return "charged weak current";
-    case simb::kNC: return "neutral weak current";
-    default:        return "unsupported (" + std::to_string(ccnc) + ")";
-  } // switch
-} // sim::DumpMCTruth::TruthCCNCname()
-
-
-//------------------------------------------------------------------------------
-std::string sim::DumpMCTruth::TruthInteractionTypeName(int type) {
-  switch (type) {
-    case simb::kUnknownInteraction            : return "unknown interaction";
-    case simb::kQE                            : return "quasi-elastic scattering";
-    case simb::kRes                           : return "resonant scattering";
-    case simb::kDIS                           : return "deep inelastic scattering";
-    case simb::kCoh                           : return "coherent scattering";
-    case simb::kCohElastic                    : return "coherent elastic scattering";
-    case simb::kElectronScattering            : return "electron scattering";
-    case simb::kIMDAnnihilation               : return "inverse muon decay annihilation";
-    case simb::kInverseBetaDecay              : return "inverse beta decay";
-    case simb::kGlashowResonance              : return "Glashow resonance";
-    case simb::kAMNuGamma                     : return "anomalous neutrino-photon interaction";
-    case simb::kMEC                           : return "meson exchange current";
-    case simb::kDiffractive                   : return "diffractive";
-    case simb::kEM                            : return "electromagnetic";
-    case simb::kWeakMix                       : return "weak mixing";
-    case simb::kNuanceOffset                  : return "<nuance offset>";
-    case simb::kCCQE                          : return "charged current quasi-elastic scattering";
-    case simb::kNCQE                          : return "neutral current quasi-elastic scattering";
-    case simb::kResCCNuProtonPiPlus           : return "resonant charged current neutrino proton pi+";
-    case simb::kResCCNuNeutronPi0             : return "resonant charged current neutrino neutron pi0";
-    case simb::kResCCNuNeutronPiPlus          : return "resonant charged current neutrino neutron pi+";
-    case simb::kResNCNuProtonPi0              : return "resonant neutral current neutrino proton pi0";
-    case simb::kResNCNuProtonPiPlus           : return "resonant neutral current neutrino proton pi+";
-    case simb::kResNCNuNeutronPi0             : return "resonant neutral current neutrino neutron pi0";
-    case simb::kResNCNuNeutronPiMinus         : return "resonant neutral current neutrino neutron pi-";
-    case simb::kResCCNuBarNeutronPiMinus      : return "resonant charged current antineutrino neutron pi-";
-    case simb::kResCCNuBarProtonPi0           : return "resonant charged current antineutrino proton pi0";
-    case simb::kResCCNuBarProtonPiMinus       : return "resonant charged current antineutrino proton pi-";
-    case simb::kResNCNuBarProtonPi0           : return "resonant neutral current antineutrino proton pi0";
-    case simb::kResNCNuBarProtonPiPlus        : return "resonant neutral current antineutrino proton pi+";
-    case simb::kResNCNuBarNeutronPi0          : return "resonant neutral current antineutrino neutron pi0";
-    case simb::kResNCNuBarNeutronPiMinus      : return "resonant neutral current antineutrino neutron pi-";
-    case simb::kResCCNuDeltaPlusPiPlus        : return "resonant charged current neutrino Delta+ pi+";
-    case simb::kResCCNuDelta2PlusPiMinus      : return "resonant charged current neutrino Delta++ pi-";
-    case simb::kResCCNuBarDelta0PiMinus       : return "resonant charged current antineutrino Delta0 pi-";
-    case simb::kResCCNuBarDeltaMinusPiPlus    : return "resonant charged current antineutrino Delta- pi+";
-    case simb::kResCCNuProtonRhoPlus          : return "resonant charged current neutrino proton rho+";
-    case simb::kResCCNuNeutronRhoPlus         : return "resonant charged current neutrino neutron rho+";
-    case simb::kResCCNuBarNeutronRhoMinus     : return "resonant charged current antineutrino neutron rho-";
-    case simb::kResCCNuBarNeutronRho0         : return "resonant charged current antineutrino neutron rho0";
-    case simb::kResCCNuSigmaPlusKaonPlus      : return "resonant charged current neutrino Sigma+ kaon+";
-    case simb::kResCCNuSigmaPlusKaon0         : return "resonant charged current neutrino Sigma+ kaon0";
-    case simb::kResCCNuBarSigmaMinusKaon0     : return "resonant charged current antineutrino Sigma- kaon0";
-    case simb::kResCCNuBarSigma0Kaon0         : return "resonant charged current antineutrino Sigma0 kaon0";
-    case simb::kResCCNuProtonEta              : return "resonant charged current neutrino proton eta";
-    case simb::kResCCNuBarNeutronEta          : return "resonant charged current antineutrino neutron eta";
-    case simb::kResCCNuKaonPlusLambda0        : return "resonant charged current neutrino Kaon+ Lambda0";
-    case simb::kResCCNuBarKaon0Lambda0        : return "resonant charged current antineutrino kaon0 Lambda0";
-    case simb::kResCCNuProtonPiPlusPiMinus    : return "resonant charged current neutrino proton pi+ pi-";
-    case simb::kResCCNuProtonPi0Pi0           : return "resonant charged current neutrino proton pi0 pi0";
-    case simb::kResCCNuBarNeutronPiPlusPiMinus: return "resonant charged current antineutrino neutron pi+ pi-";
-    case simb::kResCCNuBarNeutronPi0Pi0       : return "resonant charged current antineutrino neutron pi0 pi0";
-    case simb::kResCCNuBarProtonPi0Pi0        : return "resonant charged current antineutrino proton pi0 pi0";
-    case simb::kCCDIS                         : return "charged current deep inelastic scattering";
-    case simb::kNCDIS                         : return "neutral current deep inelastic scattering";
-    case simb::kUnUsed1                       : return "unused (1)";
-    case simb::kUnUsed2                       : return "unused (2)";
-    case simb::kCCQEHyperon                   : return "charged current quasi-elastic scattering with hyperon";
-    case simb::kNCCOH                         : return "neutral current coherent scattering";
-    case simb::kCCCOH                         : return "charged current coherent scattering";
-    case simb::kNuElectronElastic             : return "electron neutrino elastic";
-    case simb::kInverseMuDecay                : return "inverse muon decay";
-    default                                   : return "unsupported (" + std::to_string(type) + ")";
-  } // switch
-} // sim::DumpMCTruth::TruthInteractionTypeName()
-
-
-//------------------------------------------------------------------------------
-
-std::string sim::DumpMCTruth::ParticleName(int pigid) {
-  TParticlePDG const* PDGinfo = TDatabasePDG::Instance()->GetParticle(pigid);
-  return PDGinfo? PDGinfo->GetTitle(): ("PDG ID " + std::to_string(pigid));
-} // sim::DumpMCTruth::ParticleName()
 
 
 //------------------------------------------------------------------------------
