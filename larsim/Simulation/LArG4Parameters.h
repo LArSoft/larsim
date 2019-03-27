30931c11654c4a4eec0a7576e846529eec76d571
Kyle Knoepfel <knoepfel@fnal.gov>
2019-03-27 15:25:25 -0500

    Declare LArG4Parameters service to be thread-safe.

--- larsim/Simulation/LArG4Parameters.h
+++ larsim/Simulation/LArG4Parameters.h
@@ -31,8 +31,6 @@ namespace sim {
   public:
     LArG4Parameters(fhicl::ParameterSet const& pset);
 
-    void reconfigure(fhicl::ParameterSet const& pset);
-
     int    OpVerbosity()                                      const { return fOpVerbosity;            }
     double ParticleKineticEnergyCut()                         const { return fParticleKineticECut;    }
     bool   StoreTrajectories()                                const { return fStoreTrajectories;      }
@@ -57,73 +55,72 @@ namespace sim {
     int    MNXSBias()                                         const { return fXSBias;                 }
     bool   KeepEMShowerDaughters()                            const { return fKeepEMShowerDaughters;  }
     bool   DisableWireplanes()                                const { return fDisableWireplanes;      }
-    const std::vector<unsigned short int> SkipWireSignalInTPCs() const { return fSkipWireSignalInTPCs;}
-    const std::string IonAndScintCalculator()                 const { return fIonAndScintCalculator;  }
-    const std::vector<std::string> OpticalParamVolumes()      const { return fOpticalParamVolumes;    }
-    const std::vector<std::string> OpticalParamModels()       const { return fOpticalParamModels;     }
-    const std::vector<int>         OpticalParamOrientations() const { return fOpticalParamOrientations;}
-    const std::vector<std::vector<std::vector<double>>> OpticalParamParameters() const{return fOpticalParamParameters;  }
+    const std::vector<unsigned short int>& SkipWireSignalInTPCs() const { return fSkipWireSignalInTPCs;}
+    const std::string& IonAndScintCalculator()                 const { return fIonAndScintCalculator;  }
+    const std::vector<std::string>& OpticalParamVolumes()      const { return fOpticalParamVolumes;    }
+    const std::vector<std::string>& OpticalParamModels()       const { return fOpticalParamModels;     }
+    const std::vector<int>&         OpticalParamOrientations() const { return fOpticalParamOrientations;}
+    const std::vector<std::vector<std::vector<double>>>& OpticalParamParameters() const {return fOpticalParamParameters;  }
     bool UseLitePhotons()                                     const { return fLitePhotons;            }
 
-    bool   FillSimEnergyDeposits()                            const { return fFillSimEnergyDeposits;  }
-    bool   NoElectronPropagation()                            const { return fNoElectronPropagation;  }
-    bool   NoPhotonPropagation()                              const { return fNoPhotonPropagation;    }
+    bool FillSimEnergyDeposits()                            const { return fFillSimEnergyDeposits;  }
+    bool NoElectronPropagation()                            const { return fNoElectronPropagation;  }
+    bool NoPhotonPropagation()                              const { return fNoPhotonPropagation;    }
 
   private:
-    int                      fOpVerbosity;           ///< Verbosity of optical simulation - soon to be depricated
-    double                   fParticleKineticECut;   ///< Minimum energy a particle needs before asking Geant4
+    int  const               fOpVerbosity;           ///< Verbosity of optical simulation - soon to be depricated
+    double const             fParticleKineticECut;   ///< Minimum energy a particle needs before asking Geant4
                                                      ///< to track it, GeV
-    bool                     fStoreTrajectories;     ///< Whether to store full trajectories for every particle
+    bool const               fStoreTrajectories;     ///< Whether to store full trajectories for every particle
                                                      ///< simulated by Geant4
-    bool                     fDrawNeutrals;          ///< depricated
-    double                   fVisualizationEnergyCut;///< depricated, GeV
-    bool                     fUseCustomPhysics;      ///< Whether to use a custom list of physics processes
+    bool const               fDrawNeutrals;          ///< depricated
+    double const             fVisualizationEnergyCut;///< depricated, GeV
+    bool const               fUseCustomPhysics;      ///< Whether to use a custom list of physics processes
                                                      ///< or the default
-    bool                     fModifyProtonCut;       ///< Whether to enable custom ProtonCut value, needed for HadronHP
-    double                   fNewProtonCut;          ///< New Proton Cut parameter to override default in HadronHP
-    double                   fLongitudinalDiffusion; ///< Amount of diffusion in the longitudinal direction, cm^2/ns
-    double                   fTransverseDiffusion;   ///< Amount of diffusion in the transverse direction, cm^2/ns
-    double                   fElectronClusterSize;   ///< Number of ionization electrons in a given cluster
+    bool const               fModifyProtonCut;       ///< Whether to enable custom ProtonCut value, needed for HadronHP
+    double const             fNewProtonCut;          ///< New Proton Cut parameter to override default in HadronHP
+    double const             fLongitudinalDiffusion; ///< Amount of diffusion in the longitudinal direction, cm^2/ns
+    double const             fTransverseDiffusion;   ///< Amount of diffusion in the transverse direction, cm^2/ns
+    double const             fElectronClusterSize;   ///< Number of ionization electrons in a given cluster
                                                      ///< to be simulated in the readout simulation
-    int                      fMinNumberOfElCluster;  ///< Minimum number of electron clusters
-    std::vector<std::string> fEnabledPhysics;        ///< List of enabled physics processes if using Custom physics
-    int                      fK0Bias;                ///< Turns on secondary particle bias for K0, Lambda,
+    int const                fMinNumberOfElCluster;  ///< Minimum number of electron clusters
+    std::vector<std::string> const fEnabledPhysics;  ///< List of enabled physics processes if using Custom physics
+    int const                fK0Bias;                ///< Turns on secondary particle bias for K0, Lambda,
                                                      ///< neutrons in MuNuclear
-    int                      fXSBias;                ///< Turns on cross-section bian in MuNuclear
-    int                      fXBias;                 ///< Enhancement factor for cross-section bian in MuNuclear,
+    int const                fXSBias;                ///< Turns on cross-section bian in MuNuclear
+    int const                fXBias;                 ///< Enhancement factor for cross-section bian in MuNuclear,
                                                      ///< should be <= 100
-    bool                     fKeepEMShowerDaughters; ///< Whether to keep the secondary, tertiary, etc.
+    bool const               fKeepEMShowerDaughters; ///< Whether to keep the secondary, tertiary, etc.
                                                      ///< particles from an EM shower in the output
-    bool                     fDisableWireplanes;     ///< Turn of LAr sensitivity and remove charge
+    bool const               fDisableWireplanes;     ///< Turn of LAr sensitivity and remove charge
                                                      ///< drift simulation - use for running pure optical sims
-    std::vector<unsigned short int> fSkipWireSignalInTPCs;  ///< selective disabling of drift simulation
-    double                   fRecombA;               ///< Possibly override the RecombA parameter
-    double                   fRecombk;               ///< Possibly override the Recombk parameter
-    double                   fModBoxA;               ///< Possibly override the ModBoxA parameter
-    double                   fModBoxB;               ///< Possibly override the ModBoxB parameter
-    bool                     fUseModBoxRecomb;       ///< Use Modified Box model recombination instead of Birks
-    std::string              fIonAndScintCalculator; ///< Name of algorithm to use to calculate the number of
+    std::vector<unsigned short int> const fSkipWireSignalInTPCs;  ///< selective disabling of drift simulation
+    double const             fRecombA;               ///< Possibly override the RecombA parameter
+    double const             fRecombk;               ///< Possibly override the Recombk parameter
+    double const             fModBoxA;               ///< Possibly override the ModBoxA parameter
+    double const             fModBoxB;               ///< Possibly override the ModBoxB parameter
+    bool const               fUseModBoxRecomb;       ///< Use Modified Box model recombination instead of Birks
+    std::string const        fIonAndScintCalculator; ///< Name of algorithm to use to calculate the number of
                                                      ///< ionization electrons and scintillation photons
                                                      ///< for each G4 step, used by
                                                      ///< LArG4/IonizationAndScintillation.cxx
-    std::vector<std::string> fOpticalParamVolumes;   ///< List of volume names which have parameterized
-                                                     ///< optical models
-    std::vector<std::string> fOpticalParamModels;    ///< List of names of those models
-    std::vector<int>         fOpticalParamOrientations; ///< List of orientations of (eg wireplane) in each
-                                                        ///< param volume
-    std::vector<std::vector<std::vector<double> > > fOpticalParamParameters; ///< Model dependent list of
-                                                                             ///< parameters for optically
-                                                                             ///< paramaterized volumes
+    std::vector<std::string> const fOpticalParamVolumes; ///< List of volume names which have parameterized
+                                                         ///< optical models
+    std::vector<std::string> const fOpticalParamModels;  ///< List of names of those models
+    std::vector<int> const fOpticalParamOrientations; ///< List of orientations of (eg wireplane) in each
+                                                      ///< param volume
+    std::vector<std::vector<std::vector<double>>> const fOpticalParamParameters; ///< Model dependent list of
+                                                                                 ///< parameters for optically
+                                                                                 ///< parameterized volumes
 
-    bool fLitePhotons;
+    bool const fLitePhotons;
 
-    bool   fFillSimEnergyDeposits;          ///< handle to fill SimEdeps or not
-    //size_t fInitialSimEnergyDepositSize;    ///< reserve size for the edep collection in LArG4
-    bool   fNoElectronPropagation;          ///< specifically prevents electron propagation
-    bool   fNoPhotonPropagation;          ///< specifically prevents photon propagation in opfast
+    bool const fFillSimEnergyDeposits;  ///< handle to fill SimEdeps or not
+    bool const fNoElectronPropagation;  ///< specifically prevents electron propagation
+    bool const fNoPhotonPropagation;    ///< specifically prevents photon propagation in opfast
   };
 }
 
 
-DECLARE_ART_SERVICE(sim::LArG4Parameters, LEGACY)
+DECLARE_ART_SERVICE(sim::LArG4Parameters, GLOBAL)
 #endif
--- larsim/Simulation/LArG4Parameters_service.cc
+++ larsim/Simulation/LArG4Parameters_service.cc
@@ -19,62 +19,45 @@ namespace sim {
 
   //--------------------------------------------------------------------------
   LArG4Parameters::LArG4Parameters(fhicl::ParameterSet const& pset)
-  {
-    this->reconfigure(pset);
-  }
-
-  //--------------------------------------------------------------------------
-  void LArG4Parameters::reconfigure(fhicl::ParameterSet const& pset)
-  {
-
-    fOpVerbosity            = pset.get< int                      >("OpticalSimVerbosity"     );
-    fParticleKineticECut    = pset.get< double                   >("ParticleKineticEnergyCut");
-    fStoreTrajectories      = pset.get< bool                     >("StoreTrajectories"       );
-    fDrawNeutrals           = pset.get< bool                     >("VisualizeNeutrals"       );
-    fVisualizationEnergyCut = pset.get< double                   >("VisualizationEnergyCut"  );
-    fUseCustomPhysics       = pset.get< bool                     >("UseCustomPhysics"        );
-    fModifyProtonCut        = pset.get< bool                     >("ModifyProtonCut"         , false);
-    fNewProtonCut           = fModifyProtonCut? pset.get<double>("NewProtonCut") /* for HadronHP */ : 0.0;
-    fKeepEMShowerDaughters  = pset.get< bool                     >("KeepEMShowerDaughters"   );
-    fLongitudinalDiffusion  = pset.get< double                   >("LongitudinalDiffusion"   );
-    fTransverseDiffusion    = pset.get< double                   >("TransverseDiffusion"     );
-    fElectronClusterSize    = pset.get< double                   >("ElectronClusterSize"     );
-    fMinNumberOfElCluster   = pset.get< int                      >("MinNumberOfElCluster"    );
-    fEnabledPhysics         = pset.get< std::vector<std::string> >("EnabledPhysics"          );
-    fK0Bias                 = pset.get< int                      >("CosmogenicK0Bias"        );
-    fXBias                  = pset.get< int                      >("CosmogenicXSMNBiasOn"    );
-    fXSBias                 = pset.get< int                      >("CosmogenicXSMNBiasFactor");
+    : fOpVerbosity            {pset.get< int                      >("OpticalSimVerbosity"     )}
+    , fParticleKineticECut    {pset.get< double                   >("ParticleKineticEnergyCut")}
+    , fStoreTrajectories      {pset.get< bool                     >("StoreTrajectories"       )}
+    , fDrawNeutrals           {pset.get< bool                     >("VisualizeNeutrals"       )}
+    , fVisualizationEnergyCut {pset.get< double                   >("VisualizationEnergyCut"  )}
+    , fUseCustomPhysics       {pset.get< bool                     >("UseCustomPhysics"        )}
+    , fModifyProtonCut        {pset.get< bool                     >("ModifyProtonCut"         , false)}
+    , fNewProtonCut           {fModifyProtonCut? pset.get<double>("NewProtonCut") /* for HadronHP */ : 0.0}
+    , fLongitudinalDiffusion  {pset.get< double                   >("LongitudinalDiffusion"   )}
+    , fTransverseDiffusion    {pset.get< double                   >("TransverseDiffusion"     )}
+    , fElectronClusterSize    {pset.get< double                   >("ElectronClusterSize"     )}
+    , fMinNumberOfElCluster   {pset.get< int                      >("MinNumberOfElCluster"    )}
+    , fEnabledPhysics         {pset.get< std::vector<std::string> >("EnabledPhysics"          )}
+    , fK0Bias                 {pset.get< int                      >("CosmogenicK0Bias"        )}
+    , fXSBias                 {pset.get< int                      >("CosmogenicXSMNBiasFactor")}
+    , fXBias                  {pset.get< int                      >("CosmogenicXSMNBiasOn"    )}
     // First of last 3 flags above turns on secondary particle bias for
     // K0s,Lambdas,neutrons in MuNuclear.
-    // The second turns on cross-section bias in MuNuclear.
-    // The 3rd is the enhancement factor for XS bias in MuNuclear. Keep it
+    // The 2nd is the enhancement factor for XS bias in MuNuclear. Keep it
     // <=100.
-
-    fIonAndScintCalculator   = pset.get< std::string              >("IonAndScintCalculator", "Separate");
-    fDisableWireplanes       = pset.get< bool                     >("DisableWireplanes"       );
-    fSkipWireSignalInTPCs    = pset.get< std::vector<unsigned short int> >("SkipWireSignalInTPCs"      );
-    fRecombA                 = pset.get< double                   >("RecombA",util::kRecombA);
-    fRecombk                 = pset.get< double                   >("Recombk",util::kRecombk);
-    fModBoxA                 = pset.get< double                   >("ModBoxA",util::kModBoxA);
-    fModBoxB                 = pset.get< double                   >("ModBoxB",util::kModBoxB);
-    fUseModBoxRecomb         = pset.get< bool                     >("UseModBoxRecomb"         );
-    fOpticalParamVolumes     = pset.get< std::vector<std::string> >("OpticalParamVolumes"     );
-    fOpticalParamModels      = pset.get< std::vector<std::string> >("OpticalParamModels"      );
-    fOpticalParamOrientations= pset.get< std::vector<int>         >("OpticalParamOrientations");
-    fOpticalParamParameters  = pset.get< std::vector<std::vector<std::vector<double> > > >("OpticalParamParameters");
-    fLitePhotons             = pset.get< bool                     >("UseLitePhotons"       );
-
-    fFillSimEnergyDeposits   = pset.get< bool                     >("FillSimEnergyDeposits",false);
-    fNoElectronPropagation   = pset.get< bool                     >("NoElectronPropagation",false);
-    fNoPhotonPropagation     = pset.get< bool                     >("NoPhotonPropagation",false);
-
-    return;
-  }
+    // The 3rd turns on cross-section bias in MuNuclear.
+    , fKeepEMShowerDaughters  {pset.get< bool                     >("KeepEMShowerDaughters"   )}
+    , fDisableWireplanes       {pset.get< bool                     >("DisableWireplanes"       )}
+    , fSkipWireSignalInTPCs    {pset.get< std::vector<unsigned short int> >("SkipWireSignalInTPCs"      )}
+    , fRecombA                 {pset.get< double                   >("RecombA",util::kRecombA)}
+    , fRecombk                 {pset.get< double                   >("Recombk",util::kRecombk)}
+    , fModBoxA                 {pset.get< double                   >("ModBoxA",util::kModBoxA)}
+    , fModBoxB                 {pset.get< double                   >("ModBoxB",util::kModBoxB)}
+    , fUseModBoxRecomb         {pset.get< bool                     >("UseModBoxRecomb"         )}
+    , fIonAndScintCalculator   {pset.get< std::string              >("IonAndScintCalculator", "Separate")}
+    , fOpticalParamVolumes     {pset.get< std::vector<std::string> >("OpticalParamVolumes"     )}
+    , fOpticalParamModels      {pset.get< std::vector<std::string> >("OpticalParamModels"      )}
+    , fOpticalParamOrientations{pset.get< std::vector<int>         >("OpticalParamOrientations")}
+    , fOpticalParamParameters  {pset.get< std::vector<std::vector<std::vector<double> > > >("OpticalParamParameters")}
+    , fLitePhotons             {pset.get< bool                     >("UseLitePhotons"       )}
+    , fFillSimEnergyDeposits   {pset.get< bool                     >("FillSimEnergyDeposits",false)}
+    , fNoElectronPropagation   {pset.get< bool                     >("NoElectronPropagation",false)}
+    , fNoPhotonPropagation     {pset.get< bool                     >("NoPhotonPropagation",false)}
+  {}
 }
 
-
-namespace sim {
-
-  DEFINE_ART_SERVICE(LArG4Parameters)
-
-}
+DEFINE_ART_SERVICE(sim::LArG4Parameters)
