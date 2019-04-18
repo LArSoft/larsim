e4c6f2ecdfcc11ea512a85ff21d3a6a27f197c33
Kyle Knoepfel <knoepfel@fnal.gov>
2019-04-18 14:32:27 -0500

    Remove unsupported explicit template instantiations in classes.h files.

--- larsim/EventWeight/Base/classes.h
+++ larsim/EventWeight/Base/classes.h
@@ -3,8 +3,3 @@
 #include "canvas/Persistency/Common/Wrapper.h"
 
 #include "larsim/EventWeight/Base/MCEventWeight.h"
-
-template class art::Wrapper<evwgh::MCEventWeight>;
-template class art::Wrapper<std::vector<evwgh::MCEventWeight> >;
-template class std::map<std::string, double>;
-template class std::map<std::string, std::vector<double> >;
