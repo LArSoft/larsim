969b1ad63d48ec061c844e475601698dcf42e63c
Iker de Icaza Astiz <icaza@fnal.gov>
2020-04-06 22:30:24 -0500

    WIP function to create memoized functions

--- larsim/LegacyLArG4/OpFastScintillation.hh
+++ larsim/LegacyLArG4/OpFastScintillation.hh
@@ -558,6 +558,25 @@ namespace larg4 {
     return false;
   }
 
+    // template<typename Function, typename... Args>
+    // auto OpFastScintillation::invoke_memoized(Function function, Args... args)
+    // {
+    //   using key_type   = std::tuple<Args...>;
+    //   using value_type = std::invoke_result_t<Function, Args...>;
+    //   static_assert(! std::is_same_v<Function, std::function<value_type(Args...)>>,
+    //                 "cannot memoize on std::function (use a lambda instead)");
+    //   static_assert(! std::is_same_v<Function, value_type(*)(Args...)>,
+    //                 "cannot memoize on function pointer (use a lambda instead)");
+    //   static std::mutex mutex;
+    //   static std::map<key_type, value_type> cache;
+    //   auto key  = std::tuple(args...);
+    //   auto lock = std::lock_guard<std::mutex>(mutex);
+    //   if (cache.count(key)){
+    //     return cache[key];
+    //   }
+    //   return cache[key] = std::apply(function, key);
+    // }
+
 } // namespace larg4
 
 #endif /* OpFastScintillation_h */
