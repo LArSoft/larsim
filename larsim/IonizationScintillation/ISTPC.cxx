10a7c8afeb15416c72c7d788ceaccfc4d10218dc
Claudia Alvarez <claudiaalvgar@hotmail.es>
2021-06-02 10:10:42 -0500

    Hybrid Model

---
+++ larsim/IonizationScintillation/ISTPC.cxx
@@ -0,0 +1,58 @@
+#include "larsim/IonizationScintillation/ISTPC.h"
+#include "larcore/CoreUtils/ServiceUtil.h"
+
+namespace larg4 {
+  //----------------------------------------------------------------------------                 
+  ISTPC::ISTPC()
+  {
+    std::cout << "IonizationAndScintillation/ISTPC Initialize." << std::endl;
+    std::cout << "Initializing the geometry of the detector." << std::endl;
+    geo::GeometryCore const& geom = *(lar::providerFrom<geo::Geometry>());
+
+    fActiveVolumes = extractActiveVolumes(geom);
+    {
+      auto log = mf::LogTrace("IonAndScint") << "IonAndScint: active volume boundaries from "
+                                             << fActiveVolumes.size() << " volumes:";
+      for (auto const& [iCryo, box] : util::enumerate(fActiveVolumes)) {
+        log << "\n - C:" << iCryo << ": " << box.Min() << " -- " << box.Max() << " cm";
+      }
+    } // local scope        
+  }
+
+
+  //---------------------------------------
+
+
+ bool
+ ISTPC::isScintInActiveVolume(geo::Point_t const& ScintPoint)
+ {
+   //semi-analytic approach only works in the active volume                             
+   return fActiveVolumes[0].ContainsPosition(ScintPoint);
+ }
+//----------------------------------------------------------------------------                                   
+
+
+                                                                                                                 
+std::vector<geo::BoxBoundedGeo>
+ISTPC::extractActiveVolumes(geo::GeometryCore const& geom)
+{
+  std::vector<geo::BoxBoundedGeo> activeVolumes;
+  activeVolumes.reserve(geom.Ncryostats());
+
+  for (geo::CryostatGeo const& cryo : geom.IterateCryostats()) {
+
+    // can't use it default-constructed since it would always include origin                                                
+
+    geo::BoxBoundedGeo box{cryo.TPC(0).ActiveBoundingBox()};
+
+    for (geo::TPCGeo const& TPC : cryo.IterateTPCs())
+      box.ExtendToInclude(TPC.ActiveBoundingBox());
+
+    activeVolumes.push_back(std::move(box));
+
+  } // for cryostats                                                                                                         
+
+ return activeVolumes;
+}
+
+}
