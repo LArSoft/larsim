5e32e684b49e5322988b7cb4abaefa9a9c0a5c67
Daria Borodulina <dborodulina@cca020.in2p3.fr>
2025-06-10 10:57:01 +0200

    Test - header

---
+++ larsim/IonizationScintillation/IGapChargeTransport.hh
@@ -0,0 +1,21 @@
+#ifndef LARSIM_IGAPCHARGETRANSPORTTOOL_HH
+#define LARSIM_IGAPCHARGETRANSPORTTOOL_HH
+
+#include "larcorealg/Geometry/geo_vectors_utils.h"
+#include <utility>
+
+namespace gap {
+  class IGapChargeTransport {
+  public:
+    virtual ~IGapChargeTransport() = default;
+
+    /// Whether to apply gap charge transport function
+    virtual bool EnableGapChargeTransport() const = 0;
+
+    /// Return shifted position and number of moved electrons
+    virtual std::pair<geo::Point_t, int> GetOffset(
+      double x, double y, double z, int n) const = 0;
+  };
+}
+
+#endif
