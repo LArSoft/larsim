82f3e1c4bcef18ca602f734c946f5368572f33b9
Luigi Zappacosta <lzappaco@dunegpvm01.fnal.gov>
2026-06-24 12:53:40 -0500

    updated format

--- larsim/Utils/TruthMatchUtils.h
+++ larsim/Utils/TruthMatchUtils.h
@@ -43,23 +43,21 @@ namespace TruthMatchUtils {
 
   /**
  *  @brief  Check if a particle is a possible shower candidate via pID and process
- *  
+ *
  *  @param  particle particle!
  *
  *  @return true if possible candidate
  */
-  bool isShowerParticle(
-    const int trackID);
+  bool isShowerParticle(const int trackID);
 
-    /**
+  /**
  *  @brief  The G4 ID of the true particle which generated the E.M. shower that generated the particle
  *
  *  @param  trackID the particleID to be assessed
  *
  *  @return the ID of the G4 particle
  */
-  G4ID FindShowerPrimaryID(
-      const int trackID);
+  G4ID FindShowerPrimaryID(const int trackID);
 
   /**
  *  @brief  The G4 ID of the true particle which deposits the most energy in the recob::Hit
